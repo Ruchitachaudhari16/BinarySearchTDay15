d5bc44a08247300c831e0c9d8a76c3501a555719
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-03-25 07:58:28 -0400

    [Ruchita] Add: Binary search tree created

---
+++ BinarySearchDemo/BinarySearchDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace BinarySearchDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC1 Ability to create a BST byadding 56 and then adding 30 & 70 - Use INode to create My Binary
+            Console.WriteLine("Welcome to Binary Search Tree Demo!");
+            BinarySTDemo<int> binarSearchTree = new BinarySTDemo<int>(56);
+            binarSearchTree.Insert(30);
+            binarSearchTree.Insert(70);
+            binarSearchTree.Display();
+        }
+    }
+}
